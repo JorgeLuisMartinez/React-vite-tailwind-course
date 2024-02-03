b3c06c5c3c17f2ca2cd2992432c0df16f97eb83b
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2024-02-03 18:23:41 -0500

    clase 6

---
+++ src/Pages/MyOrders/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from "../../Components/Layout";
+
+function MyOrders() {
+  return (
+    <Layout>
+      My Order
+    </Layout>
+  )
+}
+
+export default MyOrders
---
+++ src/Pages/SignIn/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from "../../Components/Layout";
+
+function SignIn() {
+  return (
+    <Layout>
+      Sing In
+    </Layout>
+  )
+}
+
+export default SignIn
